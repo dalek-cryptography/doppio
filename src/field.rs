5fcc867d3c9b0aef862ff0ae72d14061301c1084
Henry de Valence <hdevalence@hdevalence.ca>
2019-04-08 18:54:37 -0700

    Add a stub of the field element API.
    This defines (but does not implement):
    
    * conversion to and from the curve25519-dalek packed `Scalar` type;
    * addition;
    * subtraction;
    * multiplication;
    * zero & one constructors and Default;
    * inversion;
    
    The implementation in curve25519-dalek allows doing operations either in
    Montgomery form or not in Montgomery form; I think that for this use-case, it
    would be better to require that a `FieldElement` is *always* in Montgomery
    form, so that the multiplication is always Montgomery multiplication.  We can
    also probably save some time by allowing the representatives to lie in the
    range [0, 2*l) instead of [0, l) as in the original implementation (cf. "Montgomery
    Multiplication Needs No Final Subtractions").

---
+++ src/field.rs
@@ -0,0 +1,81 @@
+//! Field arithmetic for the doppio curve.
+//!
+//! Because doppio is intended for use in ristretto255-based proof
+//! systems, the scalar field of ristretto255 is the *ground field*
+//! for doppio.
+//!
+//! This implementation is derived from the 52-bit scalar
+//! implementation contributed to `curve25519-dalek` by Andrew Moon.
+
+use curve25519_dalek::scalar::Scalar;
+
+use std::default::Default;
+use std::ops::{Add, Mul, Sub};
+
+/// A field element modulo \\(2\^{252} +
+/// 27742317777372353535851937790883648493\\), the ground field for
+/// the doppio curve and the scalar field for the ristretto255 group.
+#[derive(Copy, Clone, Debug)]
+pub struct FieldElement([u64; 5]);
+
+impl Add<FieldElement> for FieldElement {
+    type Output = FieldElement;
+    fn add(self, rhs: FieldElement) -> FieldElement {
+        unimplemented!();
+    }
+}
+
+impl Sub<FieldElement> for FieldElement {
+    type Output = FieldElement;
+    fn sub(self, rhs: FieldElement) -> FieldElement {
+        unimplemented!();
+    }
+}
+
+impl Mul<FieldElement> for FieldElement {
+    type Output = FieldElement;
+    fn mul(self, rhs: FieldElement) -> FieldElement {
+        unimplemented!();
+    }
+}
+
+impl Default for FieldElement {
+    fn default() -> FieldElement {
+        FieldElement::zero()
+    }
+}
+
+impl From<Scalar> for FieldElement {
+    fn from(packed: Scalar) -> FieldElement {
+        unimplemented!();
+    }
+}
+
+impl Into<Scalar> for FieldElement {
+    fn into(self) -> Scalar {
+        unimplemented!();
+    }
+}
+
+impl FieldElement {
+    pub fn zero() -> FieldElement {
+        FieldElement([0; 5])
+    }
+
+    pub fn one() -> FieldElement {
+        // This needs to return 1/R mod l
+        unimplemented!();
+    }
+
+    pub fn invert(&self) -> FieldElement {
+        unimplemented!();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
